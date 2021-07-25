02eabe675cb96f5982f54ff154e9e6fb920b2b72
nel951 <62973378+hainwoo@users.noreply.github.com>
2021-07-25 16:43:21 +0900

    Update processing.py

--- processing.py
+++ processing.py
@@ -1,15 +1,19 @@
 from konlpy.tag import Okt
-okt = Okt()
 
-text = '지금 뭐하고 있어?.'
+okt = Okt()
+text = str(input())
 text_dic = {}
 
 text_nouns = okt.nouns(text)
-
+    
 for i in range(len(text_nouns)) :
     text_dic[text_nouns[i]] = True
-
-if text_dic['뭐'] :
-    print('지금은 탄생중')
-
 print(text_dic)
+
+try :
+    if text_dic['뭐'] & text_dic['지금']:
+        answer = '지금은 탄생중'
+        print(answer)
+
+except :
+    print('뭐라고? 다시 말해줘')
